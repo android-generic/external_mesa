maurossi/mesa

android: move to python3 in all gen rules
Developers stated that meson build moved to python3 in all gen rules,
so it is safe to assume that Android building rule may move to python3 too

Here is the command used to perform the changes:

cd external/mesa
find . -type f -name "*.mk" -exec sed -i 's/$(MESA_PYTHON2)/$(MESA_PYTHON3)/g' {} +

Changes to be committed:
	modified:   src/amd/Android.common.mk
	modified:   src/amd/Android.compiler.mk
	modified:   src/amd/vulkan/Android.mk
	modified:   src/broadcom/Android.genxml.mk
	modified:   src/compiler/Android.glsl.gen.mk
	modified:   src/compiler/Android.nir.gen.mk
	modified:   src/gallium/drivers/lima/Android.mk
	modified:   src/gallium/drivers/r600/Android.mk
	modified:   src/intel/Android.compiler.mk
	modified:   src/intel/Android.genxml.mk
	modified:   src/intel/Android.isl.mk
	modified:   src/intel/Android.perf.mk
	modified:   src/intel/Android.vulkan.mk
	modified:   src/mapi/Android.mk
	modified:   src/mesa/Android.gen.mk
	modified:   src/mesa/Android.libmesa_git_sha1.mk
	modified:   src/mesa/drivers/dri/i965/Android.mk
	modified:   src/util/Android.mk
	modified:   src/vulkan/Android.mk

src/intel/Android.vulkan.mk
@@ -247,7 +247,7 @@ $(intermediates)/vulkan/anv_entrypoints.c: $(ANV_ENTRYPOINTS_GEN_SCRIPT) \
 					   $(ANV_EXTENSIONS_SCRIPT) \
 					   $(VULKAN_API_XML)
 	@mkdir -p $(dir $@)
-	$(MESA_PYTHON2) $(ANV_ENTRYPOINTS_GEN_SCRIPT) \
+	$(MESA_PYTHON3) $(ANV_ENTRYPOINTS_GEN_SCRIPT) \
 		--xml $(VULKAN_API_XML) \
 		--outdir $(dir $@)
 
@@ -257,7 +257,7 @@ $(intermediates)/vulkan/anv_extensions.c: $(ANV_EXTENSIONS_GEN_SCRIPT) \
 					  $(ANV_EXTENSIONS_SCRIPT) \
 					  $(VULKAN_API_XML)
 	@mkdir -p $(dir $@)
-	$(MESA_PYTHON2) $(ANV_EXTENSIONS_GEN_SCRIPT) \
+	$(MESA_PYTHON3) $(ANV_EXTENSIONS_GEN_SCRIPT) \
 		--xml $(VULKAN_API_XML) \
 		--out-c $@
 
@@ -265,7 +265,7 @@ $(intermediates)/vulkan/anv_extensions.h: $(ANV_EXTENSIONS_GEN_SCRIPT) \
 					   $(ANV_EXTENSIONS_SCRIPT) \
 					   $(VULKAN_API_XML)
 	@mkdir -p $(dir $@)
-	$(MESA_PYTHON2) $(ANV_EXTENSIONS_GEN_SCRIPT) \
+	$(MESA_PYTHON3) $(ANV_EXTENSIONS_GEN_SCRIPT) \
 		--xml $(VULKAN_API_XML) \
 		--out-h $@
 

android-generic/external_mesa

src/intel/Android.vulkan.mk
@@ -247,7 +247,7 @@ ANV_VK_ENTRYPOINTS_GEN_ARGS= \
 $(intermediates)/vulkan/anv_entrypoints.c: $(VK_ENTRYPOINTS_GEN_SCRIPT) \
 					   $(VULKAN_API_XML)
 	@mkdir -p $(dir $@)
-	$(MESA_PYTHON2) $(VK_ENTRYPOINTS_GEN_SCRIPT) \
+	$(MESA_PYTHON3) $(VK_ENTRYPOINTS_GEN_SCRIPT) \
 		--xml $(VULKAN_API_XML) \
 		$(ANV_VK_ENTRYPOINTS_GEN_ARGS) \
 		--out-c $@ --out-h $(dir $@)/anv_entrypoints.h