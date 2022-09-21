upcliujie/mesa

nvc0: make state handling race free
I am not entirely convinced that contexts can't mess up the state of other
contexts, but with this we at least turn down the amount of races on the
CPU side.

If we hit bugs later we can always look into it then and figure out what
to fix how.

I think we might need a better solution for it in the future as state
tracking might need to become more involved, but for now this should be
good enough.

Cc: mesa-stable
Signed-off-by: Karol Herbst <kherbst@redhat.com>

src/gallium/drivers/nouveau/nvc0/nvc0_state_validate.c
@@ -871,6 +871,7 @@ nvc0_switch_pipe_context(struct nvc0_context *ctx_to)
    struct nvc0_context *ctx_from = ctx_to->screen->cur_ctx;
    unsigned s;
 
+   simple_mtx_lock(&ctx_to->screen->state_lock);
    if (ctx_from)
       ctx_to->state = ctx_from->state;
    else
@@ -908,6 +909,7 @@ nvc0_switch_pipe_context(struct nvc0_context *ctx_to)
       ctx_to->dirty_3d &= ~NVC0_NEW_3D_ZSA;
 
    ctx_to->screen->cur_ctx = ctx_to;
+   simple_mtx_unlock(&ctx_to->screen->state_lock);
 }
 
 static struct nvc0_state_validate

android-generic/external_mesa

src/gallium/drivers/nouveau/nvc0/nvc0_state_validate.c
@@ -872,6 +872,7 @@ nvc0_switch_pipe_context(struct nvc0_context *ctx_to)
    struct nvc0_context *ctx_from = ctx_to->screen->cur_ctx;
    unsigned s;
 
+   simple_mtx_assert_locked(&ctx_to->screen->state_lock);
    if (ctx_from)
       ctx_to->state = ctx_from->state;
    else
@@ -967,6 +968,8 @@ nvc0_state_validate(struct nvc0_context *nvc0, uint32_t mask,
    uint32_t state_mask;
    int ret;
    unsigned i;
+
+   simple_mtx_assert_locked(&nvc0->screen->state_lock);
 
    if (nvc0->screen->cur_ctx != nvc0)
       nvc0_switch_pipe_context(nvc0);