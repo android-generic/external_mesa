60a63ec5e0c623d5a414217043fd0164cd53637d
hmtheboy154 <buingoc67@gmail.com>
2023-06-28 16:53:41 -0400

    Enable xmlconfig on Android
    I don't know why Mesa still block it, but let's put this into good
    use. This commit includes :
    - Remove Android availability check on xmlconfig
    - Include libxml2 into Android.mk
    - Enable xmlconfig in mesa3d_cross.mk
    - On Android, /etc/drirc file will move to /vendor/etc/drirc, while
    /usr/share/drirc.d will move to /data/vendor/drirc.d
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- android/mesa3d_cross.mk
+++ android/mesa3d_cross.mk
@@ -97,6 +97,9 @@ MESON_GEN_NINJA := \
 	-Dlmsensors=disabled                                                         \
 	-Dandroid-libbacktrace=disabled                                              \
 	-Dallow-kcmp=enabled                                                         \
+	-Dxmlconfig=enabled															 \
+	-Dsysconfdir="/vendor/etc"													 \
+	-Ddatadir="/data/vendor"													 \
 	$(BOARD_MESA3D_MESON_ARGS)                                                   \
 
 MESON_BUILD := PATH=/usr/bin:/bin:/sbin:$$PATH ninja -C $(MESON_OUT_DIR)/build
