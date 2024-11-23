56874c9ac7fdd44bf438f6ec3c5a83b1570973d6
hmtheboy154 <buingoc67@gmail.com>
2024-11-23 07:47:31 -0500

    vmwgfx: Import patches from OpenFyde (FydeOS)
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- src/gallium/drivers/svga/svga_format.c
+++ src/gallium/drivers/svga/svga_format.c
@@ -253,6 +253,8 @@ svga_translate_screen_target_format_vgpu10(enum pipe_format format)
    switch (format) {
    case PIPE_FORMAT_B8G8R8A8_UNORM:
       return SVGA3D_B8G8R8A8_UNORM;
+   case PIPE_FORMAT_R8G8B8A8_UNORM:
+      return SVGA3D_R8G8B8A8_UNORM;
    case PIPE_FORMAT_B8G8R8X8_UNORM:
       return SVGA3D_B8G8R8X8_UNORM;
    case PIPE_FORMAT_B5G6R5_UNORM:
@@ -2230,6 +2232,7 @@ svga_is_format_supported(struct pipe_screen *screen,
       /* VGPU10 formats */
       case SVGA3D_B8G8R8A8_UNORM:
       case SVGA3D_B8G8R8X8_UNORM:
+      case SVGA3D_R8G8B8A8_UNORM:
       case SVGA3D_B5G6R5_UNORM:
       case SVGA3D_B8G8R8X8_UNORM_SRGB:
       case SVGA3D_B8G8R8A8_UNORM_SRGB:
